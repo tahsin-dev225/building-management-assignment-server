adc1d288c856c090c7f1a635981c5b30cd98bba0
tahsin-dev225 <ahmedtasin225@gmail.com>
2025-01-04 17:30:56 +0600

    meybe cors policy solve

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 //middleware 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:['http://localhost:5173',"https://building-management-assignment.vercel.app"],
     credentials: true
 }));
 app.use(express.json());
